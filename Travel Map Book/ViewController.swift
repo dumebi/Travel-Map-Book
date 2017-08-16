2632008bc26fb52c7a64494fb098a03c8832876c
Jude Dike <dikejude49@gmail.com>
2017-08-16 21:48:42 +0100

    Initial Commit

---
+++ Travel Map Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel Map Book
+//
+//  Created by Kornet Project on 16/08/2017.
+//  Copyright © 2017 Kornet Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
